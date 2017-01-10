b332ca292ed8b41f3dc85d829aefd44f8709ca3e
Erica Wright and Patrick McGreevy <erica@>
2017-01-10 13:14:48 -0800

    Complete table of flashcards w/o definitions

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
-  $(".set").each(function() {
-    var set = this;
-    $(".initially-seen", this).click(function() {
-      $(".initially-seen", set).toggle();
-      $(".initially-hidden", set).toggle();
-    });
+  // $(".set").each(function() {
+  //   var set = this;
+  //   $(".initially-seen", this).click(function() {
+  //     $(".initially-seen", set).toggle();
+  //     $(".initially-hidden", set).toggle();
+  //   });
+  //
+  //   $(".initially-hidden", this).click(function() {
+  //     $(".initially-seen", set).toggle();
+  //     $(".initially-hidden", set).toggle();
+  //   });
+  // });
 
-    $(".initially-hidden", this).click(function() {
-      $(".initially-seen", set).toggle();
-      $(".initially-hidden", set).toggle();
+  $(".set").each(function() {
+    $(this).click(function() {
+      $(".initially-seen", this).toggle();
+      $(".initially-hidden", this).toggle();
     });
   });
 });
